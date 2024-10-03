2c607b8cc294c055f93a9743bcf83c25eea44e37
benedict585 <josephbenedict585@gmail.com>
2024-10-03 08:37:55 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Information Technology" << endl;
+	cout << "Bachelor in Information Technology 1st Year Block B" << endl;
+	cout << "Joseph Benedict G. Pardo" << endl;
+	cout << "P-4 Talobatib Labo Camarines Nortes" << endl;
+	return 0;
+}
