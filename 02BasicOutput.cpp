7adc7f65ec7143e822685e47ac34bfed693429c2
benedict585 <josephbenedict585@gmail.com>
2024-10-03 10:25:21 +0800

    Update 02BasicOutput.cpp

--- 02BasicOutput.cpp
+++ 02BasicOutput.cpp
@@ -7,7 +7,7 @@ int main()
 	
 	cout << "Our Lady of Lourdes College Foundation" << endl;
 	cout << "College of Information Technology" << endl;
-	cout << "Bachelor in Information Technology 1st Year Block B" << endl;
+	cout << "Bachelor of Science in Information Technology 1st Year Block B" << endl;
 	cout << "Joseph Benedict G. Pardo" << endl;
 	cout << "P-4 Talobatib Labo Camarines Nortes" << endl;
 	return 0;
